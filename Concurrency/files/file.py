d27641aebc37a1fdedf6055312b4fb10a39bb23b
Rahul Beniwal <rahul.b@lendenclub.com>
2022-06-02 09:36:56 +0530

    adding ipynbs of concurrency and types

---
+++ Concurrency/files/file.py
@@ -0,0 +1,18 @@
+from math import sqrt 
+
+def is_prime(x):
+    if x < 2:
+        return False 
+
+    if x == 2:
+        return True 
+    
+    if x % 2 == 0:
+        return False
+    
+    limit = int(sqrt(x)) + 1
+    for i in range(3, limit, 2):
+        if x % i == 0:
+            return False
+
+    return True 
